70187a9d4b748d25f891267f4b80521c6797ae4d
Agjan <248143261+J3ck1ess@users.noreply.github.com>
2026-04-08 19:01:40 +0500

    feat: improve App flow with input validation and exception handling

--- prisoner-survival-core/src/com/agzhan/prisoner/App.java
+++ prisoner-survival-core/src/com/agzhan/prisoner/App.java
@@ -1,27 +1,59 @@
 package com.agzhan.prisoner;
 
+import com.agzhan.prisoner.exception.PrisonerException;
 import com.agzhan.prisoner.model.Prisoner;
 import com.agzhan.prisoner.service.PrisonerService;
+import com.agzhan.prisoner.util.LogUtil;
 import com.agzhan.prisoner.util.PrisonerUtil;
 
 import java.util.List;
+import java.util.Scanner;
 
 // Prisoner Survival
 public class App {
     public static void main(String[] args) {
-        // 1. Generate prisoners
-        List<Prisoner> prisoners = PrisonerUtil.generatePrisoners(100);
 
-        System.out.println("Initial Prisoner List: ");
-        System.out.println(prisoners);
+        Scanner sc = new Scanner(System.in);
 
-        // 2. Implement the elimination process
-        Prisoner survivor = PrisonerService.findSurvivor(prisoners);
+        while (true) {
+            try {
+                System.out.println("====== Welcome to the Prisoner Survival System =====");
 
-        // 3. Print the result
-        System.out.println("\nThe final survivor: ");
-        System.out.println("ID: " + survivor.getId() +
-                ", Position: " + survivor.getFirstLocation());
+                // 1. Generate prisoners
+                System.out.println("Enter the number of prisoners: ");
+                int count = sc.nextInt();
+                if (count <= 0) {
+                    throw new PrisonerException("Invalid number of prisoners.");
+                }
+
+                // 2. Implement the elimination process
+                System.out.println("Enter the step for eliminate: ");
+                int step = sc.nextInt();
+                if (step <= 0) {
+                    throw new PrisonerException("Invalid step.");
+                }
+
+                List<Prisoner> prisoners = PrisonerUtil.generatePrisoners(count);
+                System.out.println("Initial prisoners: " + prisoners);
+
+                Prisoner survivor = PrisonerService.findSurvivor(prisoners, step);
+
+                // 3. Print the result
+                System.out.println("\nThe final survivor: ");
+                System.out.println("ID: " + survivor.getId() +
+                        ", First position: " + survivor.getFirstLocation());
+
+                break;
+
+            } catch (PrisonerException e) {
+                LogUtil.error(e.getMessage());
+
+            } catch (Exception e) {
+                LogUtil.error("Input error, please re-enter!");
+                sc.nextLine();
+            }
+        }
+
     }
 
 }
